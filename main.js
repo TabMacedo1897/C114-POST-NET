901984459f40e9c9a7b9eec2966bd528fdf50b3d
TabMacedo1897 <tabata.macedo1897@gmail.com>
2023-05-29 14:50:48 -0300

    Alterações para aula da Maria

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
-noseX=0;
-noseY=0;
+var mustacheImg;
+mustacheX = 0;
+mustacheY =0;
 
-function preload() {
-  clownNose = loadImage('https://i.postimg.cc/XJTmp3xx/mustache-removebg-preview.png');
+function preload(){
+   mustacheImg = loadImage("https://i.postimg.cc/XJTmp3xx/mustache-removebg-preview.png")
 }
-
-function setup() {
+function setup(){
   canvas = createCanvas(300, 300);
   canvas.center();
   video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
-
   poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
+  poseNet.on('pose', gotPoses)
 }
 
-function modelLoaded() {
-  console.log('PoseNet foi inicializado');
+function modelLoaded(){
+  console.log('PoseNet foi inicializado')
 }
-
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x-13;
-    noseY = results[0].pose.nose.y-1;
+function gotPoses(results){
+  if(results.length > 0){
+    mustacheX = results[0].pose.nose.x -15;
+    mustacheY = results[0].pose.nose.y -1;
   }
 }
-
-function draw() {
+function draw(){
   image(video, 0, 0, 300, 300);
-  image(clownNose, noseX, noseY, 30, 30);
+  image(mustacheImg, mustacheX, mustacheY, 30, 30)
 }
-
 function takeSnapshot(){    
   save('myFilterImage.png');
+}
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
